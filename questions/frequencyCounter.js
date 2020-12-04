c4bd9a33808839b18feaab2afd90482947bec4c5
Rishikesh46 <rishikesh.raut@raweng.com>
2020-12-04 17:58:43 +0530

    solutions added

--- questions/frequencyCounter.js
+++ questions/frequencyCounter.js
@@ -8,14 +8,46 @@ Count the frequency of a vowels in a given string.
 
 const frequencyCounter = (str) => {
 
-  Array.from(str)
-   .filter(letter => 'aeiou'.includes(letter)).length;
+
+   if (typeof str != "string") {
+
+      throw new Error("Invalid Input");
+    }
+    str = str.toLowerCase();
+  
+    let vowA = 0,
+      vowE = 0,
+      vowI = 0,
+      vowO = 0,
+      vowU = 0;
+      
+    for (let i = 0; i < str.length; i++) {
+      switch (str[i]) {
+        case "a":vowA++;
+          break;
+        case "e":vowE++;
+          break;
+        case "i":vowI++;
+          break;
+        case "o":vowO++;
+          break;
+        case "u":vowU++;
+         break;
+      }
+    }
+    return {
+       a: vowA, e: vowE, i: vowI, o: vowO, u: vowU 
+      };
+    
+  
+//   Array.from(str)
+//    .filter(letter => 'aeiou'.includes(letter)).length;
    
  
- console.log(frequencyCounter('abcdefghijklmnopqrstuvwxyz')); // 5
- console.log(frequencyCounter('test')); // 1
- console.log(frequencyCounter('ddd'));
- console.log(frequencyCounter("aeeeee"))
+//  console.log(frequencyCounter('abcdefghijklmnopqrstuvwxyz')); // 5
+//  console.log(frequencyCounter('test')); // 1
+//  console.log(frequencyCounter('ddd'));
+//  console.log(frequencyCounter("aeeeee"))
 
 }
 module.exports = frequencyCounter;
--- questions/stringRotation.js
+++ questions/stringRotation.js
@@ -6,35 +6,6 @@ const rotateString = (str, n, direction) => {
 //   var adjustedArray=rotateString(['N','E','S','W'],'S',1);
 // };
 
-  if (typeof str != "string") {
-
-    throw new Error("Invalid Input");
-  }
-  str = str.toLowerCase();
-
-  let vowA = 0,
-    vowE = 0,
-    vowI = 0,
-    vowO = 0,
-    vowU = 0;
-    
-  for (let i = 0; i < str.length; i++) {
-    switch (str[i]) {
-      case "a":vowA++;
-        break;
-      case "e":vowE++;
-        break;
-      case "i":vowI++;
-        break;
-      case "o":vowO++;
-        break;
-      case "u":vowU++;
-       break;
-    }
-  }
-  return {
-     a: vowA, e: vowE, i: vowI, o: vowO, u: vowU 
-    };
   
 
   }
